2d28990f25871aacd3840eb84d3e792147c795af
midilicious <szkud@szkud.net>
2022-11-27 11:13:40 -0500

    Added example and target to main

--- chpaint.sh
+++ chpaint.sh
@@ -9,12 +9,12 @@ source permisson.sh
 
 main () {
 	collect_args    $FLAGS $EXAMPLE $TARGET
-    get_mod         $MOD
-    get_owner       $OWNER
-    get_permisson   $PERMISSIONS
-    set_mod         $MOD
-    set_owner       $OWNER
-    set_permisson   $PERMISSIONS
+    get_mod         $MOD $EXAMPLE
+    get_owner       $OWNER $EXAMPLE
+    get_permisson   $PERMISSIONS $EXAMPLE
+    set_mod         $MOD $TARGET
+    set_owner       $OWNER $TARGET
+    set_permisson   $PERMISSIONS $TARGET
 }
 
 main
